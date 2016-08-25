7ec6c1df954e45665a73a45fb17691b6c53bbcb0
qiuh1016 <qiuh1016@126.com>
2016-08-25 11:21:56 +0800

    08251121

--- app/src/main/java/com/cetcme/zytyumin/FragmentPage2.java
+++ app/src/main/java/com/cetcme/zytyumin/FragmentPage2.java
@@ -3,11 +3,24 @@ package com.cetcme.zytyumin;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.InfoWindow;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.Marker;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
 
 import IconPager.BaseFragment;
 import navigationView.NavigationView;
@@ -15,21 +28,39 @@ import navigationView.NavigationView;
 /**
  * Created by qiuhong on 8/24/16.
  */
-public class FragmentPage2 extends BaseFragment {
+public class FragmentPage2 extends BaseFragment implements  BaiduMap.OnMarkerClickListener {
 
     private View view;
     private MapView mapView;
+    private BaiduMap baiduMap;
+
+    private NavigationView navigationView;
+
+    private Marker comMarker;
+    private InfoWindow mInfoWindow;
+    private boolean infoWindowIsShow = true;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_2, null, false);
+
         initNavigationView();
-        mapView = (MapView) view.findViewById(R.id.baiduMap_in_fragment_2);
+        initBaiduMap();
+
+        String shipInfo = "浙嘉渔1234" + "\n" +
+                "船东：" + "123" + "\n" +
+                "电话：" + "0754" + "\n" +
+                "负责人：" + "123" + "\n" +
+                "电话：" + "1234" + "\n" +
+                "终端序号：" + "10000001" + "\n" +
+                "是否在线：" + "是" + "\n" +
+                "是否在港：" + "是" + "\n" +
+                "伏休期起始日期：" + "1990/01/01" + "\n" +
+                "伏休期结束日期：" + "1990/01/01";
+        mapMark(new LatLng(30, 122), "浙嘉渔1234");
 
         return view;
     }
-
-    private NavigationView navigationView;
 
     private void initNavigationView() {
         navigationView = (NavigationView) view.findViewById(R.id.nav_main_in_fragment_2);
@@ -49,5 +80,79 @@ public class FragmentPage2 extends BaseFragment {
                 Log.i("main","点击了左侧按钮!");
             }
         });
+    }
+
+    private void initBaiduMap() {
+        mapView = (MapView) view.findViewById(R.id.baiduMap_in_fragment_2);
+        baiduMap = mapView.getMap();
+        baiduMap.setOnMarkerClickListener(this);
+    }
+
+    private void mapMark(LatLng latLng, String shipInfo){
+
+        mapStatus(latLng);
+
+        //定义Maker坐标点
+        LatLng point = latLng;
+        //构建Marker图标
+        BitmapDescriptor bitmap = BitmapDescriptorFactory
+                .fromResource(R.drawable.icon_point);
+        //构建MarkerOption，用于在地图上添加Marker
+        OverlayOptions option = new MarkerOptions()
+                .position(point)
+                .icon(bitmap);
+        //在地图上添加Marker，并显示
+        comMarker = (Marker) baiduMap.addOverlay(option);
+
+        //创建InfoWindow展示的view
+        Button button = new Button(getActivity());
+        button.setBackgroundResource(R.drawable.infowindow_white);
+        button.setTextSize(13);
+        button.setGravity(Gravity.CENTER);
+        button.setPadding(20,20,20,40);
+        button.setText(shipInfo);
+        button.setTextColor(0xFF7D7D7D);
+        button.setGravity(Gravity.LEFT);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                baiduMap.hideInfoWindow();
+                infoWindowIsShow = false;
+            }
+        });
+
+        if (infoWindowIsShow) {
+            //创建InfoWindow , 传入 view， 地理坐标， y 轴偏移量
+            mInfoWindow = new InfoWindow(button, point, -bitmap.getBitmap().getHeight());
+
+            //显示InfoWindow
+            baiduMap.showInfoWindow(mInfoWindow);
+            infoWindowIsShow = true;
+        }
+
+
+    }
+
+    private void mapStatus(LatLng latLng) {
+        //设置中心点 和显示范围
+        MapStatus mapStatus = new MapStatus.Builder().target(latLng).zoom(9) //15
+                .build();
+        MapStatusUpdate mapStatusUpdate = MapStatusUpdateFactory
+                .newMapStatus(mapStatus);
+        baiduMap.setMapStatus(mapStatusUpdate);
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        if (marker.equals(comMarker)) {
+            if (infoWindowIsShow) {
+                baiduMap.hideInfoWindow();
+            } else {
+                baiduMap.showInfoWindow(mInfoWindow);
+            }
+            infoWindowIsShow = !infoWindowIsShow;
+        }
+        return false;
     }
 }
